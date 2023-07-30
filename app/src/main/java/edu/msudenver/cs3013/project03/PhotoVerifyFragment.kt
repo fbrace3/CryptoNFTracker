8f31349ae4025a04f06a1c9cb4134f621eba5b13
fbrace1 <fbrace1@msudenver.edu>
2023-07-30 12:59:42 -0600

    photoVerify is working

--- app/src/main/java/edu/msudenver/cs3013/project03/PhotoVerifyFragment.kt
+++ app/src/main/java/edu/msudenver/cs3013/project03/PhotoVerifyFragment.kt
@@ -1,6 +1,9 @@
 package edu.msudenver.cs3013.project03
+
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
@@ -9,6 +12,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -16,6 +21,7 @@ class PhotoVerifyFragment : Fragment() {
 
     companion object {
         private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_CAMERA_PERMISSION = 2
     }
 
     private lateinit var capturedImage: ImageView
@@ -44,10 +50,25 @@ class PhotoVerifyFragment : Fragment() {
     }
 
     private fun takePicture() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
-            intent.resolveActivity(requireActivity().packageManager)?.let {
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        // Check if the camera permission is granted
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is already granted, proceed to take a picture
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+                intent.resolveActivity(requireActivity().packageManager)?.let {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+                }
             }
+        } else {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
         }
     }
 
@@ -70,6 +91,23 @@ class PhotoVerifyFragment : Fragment() {
             data?.extras?.get("data")?.let {
                 capturedBitmap = it as Bitmap
                 capturedImage.setImageBitmap(capturedBitmap)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission granted, proceed to take a picture
+                takePicture()
+            } else {
+                // Camera permission denied, show a message to the user (optional)
+                // You can also handle this case by disabling the capture button or requesting the permission again
             }
         }
     }
